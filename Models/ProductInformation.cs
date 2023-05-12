11001904c1e35ebec9deeeb95aa09f6db95a80ed
Malwande Mabanti <malwandem@payspace.com>
2023-05-12 09:09:35 +0200

    Add project files.

---
+++ Models/ProductInformation.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace Product_Inventory_Management_System.Models
+{
+    public class ProductInformation
+    {
+        public string Name  { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
